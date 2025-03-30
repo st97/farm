4141c86dc63803ca6077d207c413a640ba231751
st97 <joseph2717@yahoo.com>
2025-03-30 22:01:53 +0000

    Document dependencies - fixing typo

--- js/query.ts
+++ js/query.ts
@@ -10,12 +10,12 @@ export default class parser {
     _found_statements?: Record<string, string>
   ): Promise<string> {
     // 1. get user defined constants
-    const constants = {};
-    const constant = /{{([A-Za-z0-9_]+)=(.+?)}}/;
+    var constants = {};
+    var constant = /{{([A-Za-z0-9_]+)=(.+?)}}/;
     let c;
     while ((c = query.match(constant))) {
-      const c_name = c[1];
-      const c_val = c[2];
+      var c_name = c[1];
+      var c_val = c[2];
       constants[c_name] = c_val;
       // remove constant definitions
       query = query.replace(constant, "");
@@ -24,24 +24,24 @@ export default class parser {
     // 2. replace overpass turbo this.statements, user-constants and shortcuts
     this.statements = {};
     if (_found_statements) this.statements = _found_statements;
-    const statement = /{{([A-Za-z0-9_]+)(:([\s\S]*?))?}}/;
+    var statement = /{{([A-Za-z0-9_]+)(:([\s\S]*?))?}}/;
     let s;
     while ((s = query.match(statement))) {
-      const s_name = s[1];
-      const s_instr = s[3] || "";
+      var s_name = s[1];
+      var s_instr = s[3] || "";
       let s_replace = "";
       // save instructions for later
       if (this.statements[s_name] === undefined) this.statements[s_name] = "";
       this.statements[s_name] += s_instr;
       // if the statement is a shortcut, replace its content
-      const shortcut = shortcuts[s_name];
+      var shortcut = shortcuts[s_name];
       if (shortcut !== undefined) {
         // these shortcuts can also be callback functions, like {{date:-1day}}
         if (typeof shortcut === "function") {
-          const res = await new Promise<string>((resolve) =>
+          var res = await new Promise<string>((resolve) =>
             shortcut(s_instr, (s) => resolve(s))
           );
-          const seed = Math.round(Math.random() * Math.pow(2, 22)); // todo: use some kind of checksum of s_instr if possible
+          var seed = Math.round(Math.random() * Math.pow(2, 22)); // todo: use some kind of checksum of s_instr if possible
           shortcuts[`__statement__${s_name}__${seed}`] = res;
           query = query.replace(
             s[0],
@@ -52,14 +52,14 @@ export default class parser {
         } else s_replace = shortcut;
       }
       // remove statement, but preserve number of newlines
-      const lc = s_instr.split(/\r?\n|\r/).length;
+      var lc = s_instr.split(/\r?\n|\r/).length;
       query = query.replace(s[0], s_replace + Array(lc).join("\n"));
     }
     // 3. remove remaining (e.g. unknown) mustache templates:
     let m;
     while ((m = query.match(/{{[\S\s]*?}}/gm))) {
       // count lines in template and replace mustache with same number of newlines
-      const lc = m[0].split(/\r?\n|\r/).length;
+      var lc = m[0].split(/\r?\n|\r/).length;
       query = query.replace(m[0], Array(lc).join("\n"));
     }
     // return the query
